9d802a2f1ccae209c0c545c9797144f629dd6bcd
jamiedishy <jamiedishy@gmail.com>
2020-10-25 22:34:56 -0400

    Error - decrypts 7 characters

--- src/cipher.py
+++ src/cipher.py
@@ -7,85 +7,78 @@ import hashlib
 from Crypto.Cipher import AES
 from base64 import b64encode, b64decode
 
+import sys
+
+
 class CipherMethod:
     def __init__(self, key):
         self.key = key
-    
+
     def encrypt(self, plain_byte: bytes) -> bytes:
         pass
-    
+
     def decrypt(self, encrypted_byte: bytes) -> bytes:
         pass
+
 
 class XORCipher(CipherMethod):
     def __init__(self, key):
         super().__init__(key)
-        # TODO
-       
-        self.key = key.encode('ASCII')#key string to bytes
+        self.key = key.encode('utf-8')  # key string to bytes
+
     def encrypt(self, plain_byte: bytes) -> bytes:
         print("encrypting")
-       # plain_string = plain_byte.decode('ASCII')
-        #encrypted_string = chr(ord(plain_string) ^ ord(self.key))
-        #encrypted_byte = encrypted_string.encode('ASCII')
-        #encrypted_byte = int.to_bytes(encrypted_int, byteorder="big")
-       # encrypted_byte = [chr(ord(a) ^ ord(b)) for (a,b) in zip(plain_byte, cycle(self.key))]
+        # plain_int = int.from_bytes(plain_byte, byteorder="big")
+        # key_int = int.from_bytes(self.key, byteorder="big")
+        # result_int = plain_int ^ key_int
+        # result_string = str(result_int)
+        # encrypted_byte = result_string.encode('utf-8')
+        # return encrypted_byte
+        return bytes([_a ^ _b for _a, _b in zip(plain_byte, self.key)])
 
-
-        plain_int = int.from_bytes(plain_byte, byteorder="big")
-        key_int = int.from_bytes(self.key, byteorder="big")
-        result_int = plain_int ^ key_int
-        result_string = str(result_int)
-        encrypted_byte = result_string.encode('ASCII')
-        return encrypted_byte
     def decrypt(self, encrypted_byte: bytes) -> bytes:
         print("decrypting")
-        #encrypted_string = encrypted_byte.decode('ASCII')
-        #plain_string = chr(ord(encrypted_string) ^ ord(self.key))
-        #plain_byte = plain_string.encode('ASCII')
-        
-        #plain_byte = [chr(ord(a) ^ ord(b)) for (a,b) in zip(encrypted_byte, cycle(self.key))]
-       
-       # plain_byte = int.to_bytes(plain_int, byteorder ="big")
+        # encrypted_int = int.from_bytes(encrypted_byte, byteorder="big")
+        # key_int = int.from_bytes(self.key, byteorder="big")
+        # result_int = encrypted_int ^ key_int
+        # result_string = str(result_int)
+        # plain_byte = result_string.encode('utf-8')
+        # return plain_byte
+        return bytes([_a ^ _b for _a, _b in zip(encrypted_byte, self.key)])
 
 
-        encrypted_int = int.from_bytes(encrypted_byte, byteorder="big")
-        key_int = int.from_bytes(self.key, byteorder="big")
-        result_int = key_int ^ encrypted_int
-        result_string = str(result_int)
-        plain_byte = result_string.encode('ASCII') 
-        return plain_byte
-        
 class AESCipher(CipherMethod):
     def __init__(self, key):
         super().__init__(key)
         # TODO
-    
+
     def encrypt(self, plain_byte: bytes) -> bytes:
         # print("encrypting")
-        encrypted_byte = plain_byte # TODO, encrypt plain_byte
+        encrypted_byte = plain_byte  # TODO, encrypt plain_byte
         return encrypted_byte
-    
+
     def decrypt(self, encrypted_byte: bytes) -> bytes:
         # print("decrypting")
-        plain_byte = encrypted_byte # TODO, decrypt plain_byte
+        plain_byte = encrypted_byte  # TODO, decrypt plain_byte
         return plain_byte
-        
+
+
 class RC4Cipher(CipherMethod):
     def __init__(self, key):
         super().__init__(key)
         # TODO
-    
+
     def encrypt(self, plain_byte: bytes) -> bytes:
         # print("encrypting")
-        encrypted_byte = plain_byte # TODO, encrypt plain_byte
+        encrypted_byte = plain_byte  # TODO, encrypt plain_byte
         return encrypted_byte
-    
+
     def decrypt(self, encrypted_byte: bytes) -> bytes:
         # print("decrypting")
-        plain_byte = encrypted_byte # TODO, decrypt plain_byte
+        plain_byte = encrypted_byte  # TODO, decrypt plain_byte
         return plain_byte
-        
+
+
 class BlowFishCipher(CipherMethod):
     def __init__(self, key):
         super().__init__(key)
@@ -93,10 +86,10 @@ class BlowFishCipher(CipherMethod):
         self.iv = Random.new().read(self.block_size)
         byte_key = str.encode(key)
         byte_key = hashlib.sha256(key.encode()).digest()
-        self.cipher = Blowfish.new(byte_key, 
-                                   Blowfish.MODE_CBC, 
+        self.cipher = Blowfish.new(byte_key,
+                                   Blowfish.MODE_CBC,
                                    self.iv)
-    
+
     def encrypt(self, plain_byte: bytes) -> bytes:
         # print("encrypting")
         plen = self.block_size - divmod(len(plain_byte), self.block_size)[1]
@@ -104,12 +97,13 @@ class BlowFishCipher(CipherMethod):
         padding = pack('b'*plen, *padding)
         encrypted_byte = self.iv + self.cipher.encrypt(plain_byte + padding)
         return encrypted_byte
-    
+
     def decrypt(self, encrypted_byte: bytes) -> bytes:
         # print("decrypting")
         ciphertext = encrypted_byte
         ciphertext = ciphertext[self.block_size:]
         plain_byte = self.cipher.decrypt(ciphertext)
         last_byte = plain_byte[-1]
-        plain_byte = plain_byte[:- (last_byte if type(last_byte) is int else ord(last_byte))]
+        plain_byte = plain_byte[:- (last_byte if type(last_byte)
+                                    is int else ord(last_byte))]
         return plain_byte
