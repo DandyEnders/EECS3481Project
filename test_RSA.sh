cd04cdeb8cbd241120cff63e750ef16d619954da
jamiedishy <jamiedishy@gmail.com>
2020-11-21 20:05:56 -0500

    RSA - encryption decryption working

--- test_RSA.sh
+++ test_RSA.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Run this by "./test.sh"
+py ./src/rsa_create_keys.py
 py run.py --directory ./data --type rsa --secrets EECS3481 --action encrypt
 py run.py --directory ./data_result --type rsa --secrets EECS3481 --action decrypt
 
